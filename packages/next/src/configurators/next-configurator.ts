13997f5ecbf7744f9adcc9fdfd37fb26bf6e2442
Karibash <y.karibayashi@gmail.com>
2025-11-20 16:49:50 +0900

    bugfix: Fix a bug that caused module resolution to fail

--- packages/next/src/configurators/next-configurator.ts
+++ packages/next/src/configurators/next-configurator.ts
@@ -1,9 +1,10 @@
-import { configs as nextConfigs } from '@next/eslint-plugin-next';
+import next from '@next/eslint-plugin-next';
 
 import type { Configurator } from '@praha/eslint-config-definer';
 
 export const nextConfigurator: Configurator = () => {
   return [
-    nextConfigs['core-web-vitals'],
+    // eslint-disable-next-line import-x/no-named-as-default-member
+    next.configs['core-web-vitals'],
   ];
 };
